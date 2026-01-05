0d2fad431ed0eee60566847a7517505b055636d0
BriseDeFeuilles <brisedefeuilles@gmail.com>
2026-01-05 16:13:06 +0100

    Add files via upload

---
+++ Client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	HOST = "localhost"
+	PORT = "8080"
+	TYPE = "tcp"
+)
+
+func main() {
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
+
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP(TYPE, nil, tcpServer)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	var chemin string
+	fmt.Print("Chemin de l'image : ")
+	fmt.Scan(&chemin)
+
+	_, err = conn.Write([]byte(chemin))
+	if err != nil {
+		println("Write data failed:", err.Error())
+		os.Exit(1)
+	}
+
+	// buffer to get data
+	received := make([]byte, 1024)
+	_, err = conn.Read(received)
+	if err != nil {
+		println("Read data failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("Received message:", string(received))
+
+	conn.Close()
+}
