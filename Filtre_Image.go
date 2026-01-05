0dcf53b3c2073299ff59b6fb42353eb231d1d812
BrumeMer51 <laoipub51+github@gmail.com>
2026-01-05 14:38:30 +0100

    Ajout de la division en bandes sur Filtre_Image.go

--- Filtre_Image.go
+++ Filtre_Image.go
@@ -9,6 +9,19 @@ func main() {
 	n := 4
 	// Import de l'image
 	// Récupération des dimensions
-	// Division de l'image en n bande 
+	// Division de l'image en n bande
+	intervale := bounds.Max.Y % n
+	b_inf := 0
+	b_sup := 0
 	// Pour chaque bande lancer la go routine
+	for i := range n {
+		b_inf = b_inf + intervale
+		if i != n-1 {
+			b_sup = b_inf + intervale
+		} else {
+			b_sup = bounds.Max.Y
+		}
+
+		go traitement_bande(paramètres)
+
 }
