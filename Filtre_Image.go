640282b3366f765331562d6451366534c2a4004e
BrumeMer51 <laoipub51+github@gmail.com>
2026-01-05 14:25:19 +0100

    Add files via upload

---
+++ Filtre_Image.go
@@ -0,0 +1,14 @@
+package main
+
+// Definition de la fonction traitant une certaine bande d'image et renvoyant la bande filtrée
+func traitement_bande(borne_sup int, borne_inf int, "canal chan"  ) {
+	// Code traitement image
+}
+
+func main() {
+	n := 4
+	// Import de l'image
+	// Récupération des dimensions
+	// Division de l'image en n bande 
+	// Pour chaque bande lancer la go routine
+}
