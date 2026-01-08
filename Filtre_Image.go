b009980014973a09c8a32d199053d6e1516de73a
BriseDeFeuilles <brisedefeuilles@gmail.com>
2026-01-08 09:23:57 +0100

    ajout de commentaires

--- Filtre_Image.go
+++ Filtre_Image.go
@@ -9,31 +9,44 @@ import (
 	"sync"
 )
 
-// Definition de la fonction traitant une certaine bande d'image et renvoyant la bande filtrée
+// Definition de la fonction traitant une certaine bande d'image et crée la bande filtrée
 func traitement_bande(borne_sup int, borne_inf int, bounds image.Rectangle, imageI image.Image, imageF *image.RGBA, wg *sync.WaitGroup) {
+	/*
+		entrées :
+			borne_sup, borne_inf : bornes des lignes de l'image à modifier dans cette goroutine
+			bounds : bornes de l'image entière
+			imageI : image source dont on veut obtenir l'image filtré
+			imageF : image qui sera modifié lors de la fonction, de la même taille que imageI
+		sorties :
+			None
+	*/
 	defer wg.Done()
-	// Code traitement image
+	// Code du traitement et du filtre de l'image
+	// Pour tous les pixels
 	for y := borne_inf; y < borne_sup; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, g, b, a := imageI.At(x, y).RGBA()
-			n := (r + g + b) / 3
-
+			// Récupération de la couleur initial du pixel dans l'image initial
 			// RGBA() retourne des valeurs sur 16 bits (0–65535)
+			r, g, b, a := imageI.At(x, y).RGBA()
+
+			// Obtention de la valeur filtré en gris sur 16 bit
+			n := (r + g + b) / 3
 			// Conversion en 8 bits (0–255)
 			n8 := uint8(n >> 8)
 			a8 := uint8(a >> 8)
 
 			couleur := color.RGBA{n8, n8, n8, a8}
+			// Modification de la couleur du pixel sur l'image final par la couleur filtré
 			imageF.Set(x, y, couleur)
-
 		}
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
+	// nombre de goroutines à utilisé
 	n := 4
-	// Import de l'image
+	// Import de l'image à partir du chemin
 	file, err := os.Open("deepfield.png")
 	if err != nil {
 		log.Fatal(err)
@@ -45,13 +58,13 @@ func main() {
 		log.Fatalf("Error reading file: %v", err)
 	}
 
-	// Récupération des dimensions
+	// Récupération des dimensions de l'image
 	bounds := imageI.Bounds()
 
 	// Création de la nouvelle image
 	imageF := image.NewRGBA(image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y))
 
-	// Division de l'image en n bande
+	// Calcul des bornes de la premier bande de l'image pour n bande et de l'intervale de chaque bande
 	intervale := bounds.Max.Y / n
 	b_inf := 0
 	b_sup := intervale
@@ -80,8 +93,3 @@ func main() {
 		log.Fatalf("Error encoding image: %v", err)
 	}
 }
-
-
-
-
-
