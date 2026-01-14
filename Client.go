b5cd02c7c6b056013477898280d2a57fa4cb4bfb
BrumeMer51 <laoipub51+github@gmail.com>
2026-01-14 11:42:41 +0100

    Ajout du choix du filtre sur Client.go

--- Client.go
+++ Client.go
@@ -10,38 +10,37 @@ import (
 // Constantes liées au serveur auquel on doit se connecter
 const (
 	HOST = "localhost"
-	PORT = "8080"
+	PORT = "7021"
 	TYPE = "tcp"
 )
 
 func main() {
-	//Cette fonction renvoie l'adresse du serveur TCP pour s'y connecter
-	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT) 
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT) //Cette fonction renvoie l'adresse du serveur TCP pour s'y connecter
 
 	if err != nil {
 		println("ResolveTCPAddr failed:", err.Error())
 		os.Exit(1)
 	}
 
-	//Cette fonction permet de se connecter au serveur donné en paramètre
-	conn, err := net.DialTCP(TYPE, nil, tcpServer) 
+	conn, err := net.DialTCP(TYPE, nil, tcpServer) //Cette fonction permet de se connecter au serveur donné en paramètre
 	if err != nil {
 		println("Dial failed:", err.Error())
 		os.Exit(1)
 	}
 
-	// Demande le chemin de l'image sur la machine
 	var chemin string
 	fmt.Print("Chemin de l'image : ")
 	fmt.Scan(&chemin)
 
-	// Demande le nom de la nouvelle image à créer
 	var titre string
 	fmt.Print("Titre de la nouvelle image (sans extension) : ")
 	fmt.Scan(&titre)
 
-	// Prépare le message à envoyer au serveur contenant les informations des noms d'image
-	message := chemin + "," + titre + ".png"
+	var filtre string
+	fmt.Print("Filtre à utilisé : 1 (N&B) ou 2 (Flou gaussien)")
+	fmt.Scan(&filtre)
+
+	message := chemin + "," + titre + ".png" + "," + filtre
 
 	_, err = conn.Write([]byte(message))
 	if err != nil {
@@ -57,9 +56,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Affiche le message de réponse renvoyé par le serveur rapportant sur l'execiton de la fonction
 	println("Message reçu:", string(received))
 
 	conn.Close()
 }
-
