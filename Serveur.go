d860c72fdcdf8dc11a68e582b8462ef36096c79f
BrumeMer51 <laoipub51+github@gmail.com>
2026-01-14 11:42:16 +0100

    Ajout du flou gaussien sur Serveur.go

--- Serveur.go
+++ Serveur.go
@@ -6,19 +6,102 @@ import (
 	"image/color"
 	"image/png"
 	"log"
+	"math"
 	"net"
 	"os"
 	"strings"
 )
 
-// Constantes liées à la socket utilisée par le serveur :
+// Constantes liées à la socket utilisée par le serveur et à la taille de la matrice du flou gaussien :
 const (
-	HOST = "localhost"
-	PORT = "8080"
-	TYPE = "tcp"
+	HOST   = "localhost"
+	PORT   = "7021"
+	TYPE   = "tcp"
+	taille = 21
 )
 
-func traitement_image(chemin_image string, titre string) {
+func creation_matrice() [taille][taille]float64 {
+	var tab [taille][taille]float64
+	sigma := float64((taille - 1)) / 6.0
+	centre := float64((taille - 1)) / 2.0
+
+	somme := 0.0
+	// Calcul de tous les coefficients
+	for i := 0; i < taille; i++ {
+		for j := 0; j < taille; j++ {
+			tab[i][j] = (1 / (2 * math.Pi * math.Pow(sigma, 2))) * math.Exp((-math.Pow(centre-float64(i), 2)-math.Pow(centre-float64(j), 2))/2*math.Pow(sigma, 2))
+			somme += tab[i][j]
+		}
+	}
+
+	// Normalisation des coefficients
+	for i := 0; i < taille; i++ {
+		for j := 0; j < taille; j++ {
+			tab[i][j] = tab[i][j] / somme
+		}
+	}
+
+	return tab
+}
+
+func filtreNoirBlanc(bounds image.Rectangle, imageI image.Image, imageF *image.RGBA) {
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := imageI.At(x, y).RGBA()
+			n := (r + g + b) / 3
+
+			// RGBA() retourne des valeurs sur 16 bits (0–65535)
+			// Conversion en 8 bits (0–255)
+			n8 := uint8(n >> 8)
+			a8 := uint8(a >> 8)
+
+			couleur := color.RGBA{n8, n8, n8, a8}
+			imageF.Set(x, y, couleur)
+
+		}
+	}
+
+}
+
+func filtreFlouGaussien(bounds image.Rectangle, imageI image.Image, imageF *image.RGBA, noyau [taille][taille]float64) {
+	taille := len(noyau)
+	// Pour tous les pixels sauf les bords
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		if bounds.Min.Y+taille < y && bounds.Max.Y-taille > y {
+			for x := bounds.Min.X; x < bounds.Max.X; x++ {
+				if bounds.Min.X+taille < x && bounds.Max.X-taille > x {
+					R := 0.0
+					G := 0.0
+					B := 0.0
+					// Pour tous les voisins dans le noyau :
+					for i := 0; i < taille; i++ {
+						for j := 0; j < taille; j++ {
+							// Si on est pas sur le centre :
+							if i != taille && j != taille {
+								// Récupération de la couleur initial du pixel dans l'image initiale, et calcul du flou
+								r, g, b, _ := imageI.At(x, y).RGBA()
+								R += float64(r) * noyau[i][j]
+								G += float64(g) * noyau[i][j]
+								B += float64(b) * noyau[i][j]
+							}
+						}
+					}
+					// Conversion en 8 bits (0–255)
+					R8 := uint8(R)
+					G8 := uint8(G)
+					B8 := uint8(B)
+					a8 := uint8(255)
+
+					couleur := color.RGBA{R8, G8, B8, a8}
+					// Modification de la couleur du pixel sur l'image final par la couleur filtré
+					imageF.Set(x, y, couleur)
+				}
+			}
+		}
+	}
+}
+
+func traitement_image(chemin_image string, titre string, filtre string) {
 	/*
 		Fonction prenant en paramètre le chemin local vers une image, et créant une nouvelle image avec un filtre noir et blanc
 		Entrées :
@@ -42,30 +125,18 @@ func traitement_image(chemin_image string, titre string) {
 		log.Fatalf("Error reading file: %v", err)
 	}
 
-	// Récupération des dimensions de l'image
+	// Récupération des dimensions
 	bounds := imageI.Bounds()
 
 	// Création de la nouvelle image
 	imageF := image.NewRGBA(image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y))
 
 	// Traitement de l'image
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			// Récupération de la couleur initial du pixel dans l'image initial
-			// RGBA() retourne des valeurs sur 16 bits (0–65535)
-			r, g, b, a := imageI.At(x, y).RGBA()
-
-			// Obtention de la valeur filtré en gris sur 16 bit
-			n := (r + g + b) / 3
-			// Conversion en 8 bits (0–255)
-			n8 := uint8(n >> 8)
-			a8 := uint8(a >> 8)
-
-			couleur := color.RGBA{n8, n8, n8, a8}
-			// Modification de la couleur du pixel sur l'image final par la couleur filtré
-			imageF.Set(x, y, couleur)
-
-		}
+	if filtre == "1" {
+		filtreNoirBlanc(bounds, imageI, imageF)
+	} else {
+		noyau := creation_matrice()
+		filtreFlouGaussien(bounds, imageI, imageF, noyau)
 	}
 
 	// Finalisation de l'image
@@ -100,12 +171,13 @@ func handleRequest(conn net.Conn) {
 
 	// Sélection de l'image à traiter, récupération du titre de la nouvelle image et traitement
 	recu := string(buffer[:n])
-	chemin, titre, ok := strings.Cut(recu, ",")
+	morceaux := strings.Split(recu, ",")
 
-	chemin = strings.TrimSpace(chemin)
-
-	if ok == true {
-		traitement_image(chemin, titre)
+	if len(morceaux) == 3 {
+		chemin := strings.TrimSpace(morceaux[0])
+		titre := strings.TrimSpace(morceaux[1])
+		filtre := strings.TrimSpace(morceaux[2])
+		traitement_image(chemin, titre, filtre)
 
 		responseStr := "Image traitée"
 		conn.Write([]byte(responseStr))
@@ -123,9 +195,8 @@ func main() {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	// Lancement de l'écoute de manière infinie, quand une connexion est demandée de la part d'un client, une go routine est lancée
-	// La fermeture de l'écoute est différée au moment où le serveur sera fermé
-	defer listen.Close() 
+	// Lancement de l'écoute de manière infinie, quand une connexion est demandée de la part d'un client, une goroutine est lancée
+	defer listen.Close() //La fermeture de l'écoute est différée au moment où le serveur sera fermé
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
@@ -135,4 +206,3 @@ func main() {
 		go handleRequest(conn)
 	}
 }
-
