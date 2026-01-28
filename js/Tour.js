0b43b771b6879def8f15b5130f9926effb1585ef
BrumeMer51 <laoipub51+github@gmail.com>
2026-01-28 11:48:03 +0100

    Update Tour.js

--- js/Tour.js
+++ js/Tour.js
@@ -4,4 +4,23 @@ export class Tour {
         this.actions = [];
         this.modificateur = [];
     }
+
+    somme() {
+        let res_nb = 0
+        let res_bonus = 0
+        // On fait la somme des nombres :
+        for (const nb in this.nombres){
+            res_nb += parseInt(nb, 10)
+        }
+        for (const modif in this.modificateur){
+            if (modif != "x2") {
+                let ajout = modif.slice(1)
+                res_bonus += ajout
+            }
+            else {
+                res_nb = res_nb*2
+            }
+        }
+        return res_nb + res_bonus
+    }
 }
